72b7b89eb0813a073118af34b79145239d633ee4
Mariano Perin <mariano.perin@gmail.com>
2021-09-13 16:27:34 -0300

    seccion donde se encuentran los articulos agregada

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 class Articulo {
-    constructor(nombre) {
+    constructor(nombre, seccion) {
         this.nombre = nombre.toUpperCase();
+        this.seccion = seccion.toUpperCase();
     }
 }
 
@@ -9,9 +10,8 @@ const articulos = []
 const recuperar = JSON.parse(localStorage.getItem("arrayArticulosJSON"));
 
 // AGREGO UN IF PORQUE AL PRINCIPIO NO HAY NADA CARGADO
-
 if (recuperar) for (let objeto of recuperar){
-    articulos.push(new Articulo (`${objeto.nombre}`));
+    articulos.push(new Articulo (`${objeto.nombre}`, `${objeto.seccion}`));
 }
 
 impresionArticulos();
@@ -25,8 +25,16 @@ $('#limpiar-btn').on("click", limpiarArticulos);
 // FUNCION PARA "ACTUALIZAR" LOS ARTICULOS DE LA LISTA
 function impresionArticulos(){
     $('#supermercado-list').empty();
+    // ENCABEZADO DE LOS ARTICULOS Y SECCION
+    $('#supermercado-list').append(`<div class="row justify-content-between px-3">
+                                        <h5 class="bar-title">Artículo</h5>
+                                        <h5 class="bar-section">Sección</h5>
+                                    </div>`);
     for (let i = 0; i < articulos.length; i++){
-        $('#supermercado-list').append(`<h5 class="item-title">${articulos[i].nombre}</h5>`);
+        $('#supermercado-list').append(`<div class="row justify-content-between px-3">
+                                            <h5 class="item-title">${articulos[i].nombre}</h5>
+                                            <h5 class="item-section">${articulos[i].seccion}</h5>
+                                        </div>`);
     }
 }
 
@@ -34,16 +42,17 @@ function impresionArticulos(){
 function agregarArticulo(evt) {
     evt.preventDefault();
     let obtenerDato = $(evt.target).children();
-    articulos.push(new Articulo (`${obtenerDato[0].value}`));
+    articulos.push(new Articulo (`${obtenerDato[0].value}`,`${obtenerDato[1].value}`));
     guardar("arrayArticulosJSON", JSON.stringify(articulos));
     // LO UTILIZO PARA MIRAR COMO EL EVENTO ACTUALIZA EL ARRAY
     console.table(articulos)
     // LIMPIO EL INPUT Y LO VUELVO A PONER EN FOCUS PARA SEGUIR LISTANDO ARTICULOS
-    function clearInput() {
-        $("#input").val("");
-        $("#input").focus();
+    function clearInputs() {
+        $("#articulo").val("");
+        $("#seccion").val("");
+        $("#articulo").focus();
     }
-    clearInput();
+    clearInputs();
     // INVOCO LA FUNCION QUE AGREGA LOS ARTICULOS AL HTML
     impresionArticulos();
 }
